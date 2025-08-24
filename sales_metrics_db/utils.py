db94d5a31abafb5a00e6487a50586f2a93df6bcb
Bright-nwala <nwalabrightcz@gmail.com>
2025-08-24 01:53:19 +0100

    Update utils.py

--- sales_metrics_db/utils.py
+++ sales_metrics_db/utils.py
@@ -2,8 +2,9 @@ import pandas as pd
 import os
 
 def load_data(filename="cleaned_data.csv"):
-    # Build path relative to this file (utils.py)
-    base_dir = os.path.dirname(__file__)  # folder where utils.py is located
-    data_path = os.path.join(base_dir, "data", filename)
+    base_dir = os.path.dirname(__file__)  # path to sales_metrics_db/
+    data_path = os.path.join(base_dir, "data", filename)  
     return pd.read_csv(data_path)
+
+
 
